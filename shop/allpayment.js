bf2a37f912e42431f4324ee9fc898de2b9e97e39
RambotzId <rarfarter@gmail.com>
2022-02-18 11:03:24 +0800

    Alphabot

---
+++ shop/allpayment.js
@@ -0,0 +1,35 @@
+const allpayment = (prefix) => {
+return `PAYMENT BY RAMZOFC
+
+*🏧 BANK*
+
+1. BRI 
+	ㅁ -
+2. BCA 
+	ㅁ -
+3. BNI 
+	ㅁ -
+4. PERMATA
+	ㅁ -
+	
+*💰 E-MONEY*
+
+1. GOPAY
+	ㅁ Scan Via Qr
+2. OVO
+	ㅁ -
+3. DANA
+	ㅁ -
+	
+Semuanya atas nama RI****DI
+
+*📲 PULSA*
+
+Tidak tersedia
+	
+Sebelum melakukan pembayaran ada baiknya anda menghubungi owner terlebih dahulu!
+`
+	}
+
+exports.allpayment = allpayment
+ 
---
+++ shop/grupbot.js
@@ -0,0 +1,18 @@
+// jangan di ubah jika tidak punya group bot, takutnya eror
+const gcbotwa = () =>{
+	return`Join Aja Semua Fitur Bot Bisa Digunakan !
+
+1. *RamzOfc Support*
+_🔒 Privat_
+2. *RamzOfc Support 2*
+_🔒 Privat_
+3. *RamzOfc Support 3*
+_🔒 Privat_
+4. *RamzOfc Support 4* 
+_🔒 Privat_
+
+Jika ada link yang ke reset, silahkan hubungi
+owner untuk meminta link yang baru
+`
+}
+exports.gcbotwa = gcbotwa
